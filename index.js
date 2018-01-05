c77ebea5eaacf3e9e2b7c72568e54b43e7aebb5f
Emily Horsman <me@emilyhorsman.com>
2018-01-05 14:00:47 -0500

    Update visjs DataSets from UnionFind algorithm

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 import { DataSet, Network } from 'vis'
 
 
-const nodes = new DataSet([
-	{ id: 0, label: '0' },
-	{ id: 1, label: '1' },
-	{ id: 2, label: '2' },
-	{ id: 3, label: '3' },
-	{ id: 4, label: '4' },
-	{ id: 5, label: '5' },
-	{ id: 6, label: '6' },
-	{ id: 7, label: '7' },
-	{ id: 8, label: '8' },
-	{ id: 9, label: '9' },
-]);
+class WeightedQuickUnionUF {
+	constructor(n, nodes, edges) {
+		this.nodes = nodes;
+		this.edges = edges;
+		this.id = [];
+		this.sz = [];
+		this.count = n;
+		for (let i = 0; i < n; i++) {
+			this.id[i] = i;
+			this.sz[i] = 1;
+			nodes.add({ id: i, label: i.toString() });
+			edges.add({ id: i, arrows: { to: { enabled: true } } });
+		}
+	}
+
+	find(p) {
+		while (p != this.id[p]) {
+			p = this.id[p];
+		}
+		return p;
+	}
+
+	union(p, q) {
+		const i = this.find(p);
+		const j = this.find(q);
+		if (i === j) {
+			return;
+		}
+
+		if (this.sz[i] < this.sz[j]) {
+			this.edges.update({ id: i, from: i, to: j });
+			this.id[i] = j;
+			this.sz[j] += this.sz[i];
+		} else {
+			this.edges.update({ id: j, from: j, to: i });
+			this.id[j] = i;
+			this.sz[i] += this.sz[j];
+		}
+
+		this.count--;
+	}
+}
+
+const nodes = new DataSet();
 const edges = new DataSet();
-edges.add([
-	{ from: 6, to: 5, arrows: { to: { enabled: true } } },
-	{ from: 5, to: 0, arrows: { to: { enabled: true } } },
-	{ from: 2, to: 1, arrows: { to: { enabled: true } } },
-	{ from: 7, to: 1, arrows: { to: { enabled: true } } },
-	{ from: 4, to: 3, arrows: { to: { enabled: true } } },
-	{ from: 3, to: 8, arrows: { to: { enabled: true } } },
-	{ from: 9, to: 8, arrows: { to: { enabled: true } } },
-	{ from: 0, to: 1, arrows: { to: { enabled: true } } }
-]);
+const uf = new WeightedQuickUnionUF(10, nodes, edges);
+
 const data = { nodes: nodes, edges: edges };
 const options = {
 	layout: {
@@ -36,3 +60,16 @@ const options = {
 }
 const container = document.querySelector('.app');
 const network = new Network(container, data, options);
+
+
+uf.union(4, 3);
+uf.union(3, 8);
+uf.union(6, 5);
+uf.union(9, 4);
+uf.union(2, 1);
+uf.union(8, 9);
+uf.union(5, 0);
+uf.union(7, 2);
+uf.union(6, 1);
+uf.union(1, 0);
+uf.union(6, 7);
